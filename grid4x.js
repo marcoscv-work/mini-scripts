a1b1299dee0d43a6c4f35e0e7162b60d6b98d648
Marcos <marcos.castro@liferay.com>
2015-04-10 13:03:56 +0200

    no message

--- grid4x.js
+++ grid4x.js
@@ -5,7 +5,7 @@ window.toggleVerticalGrid = function(){
   	verticalGridNode.remove();
   }
   else {
-  	A.getBody().prepend('<div id="verticalGrid" style="position:fixed;top:0;left:0;right:0;bottom:0px;z-index:9999;background:https://rawgit.com/marcoscv-work/mini-scripts/master/img/4xGridBlue.png"></div>');
+  	A.getBody().prepend('<div id="verticalGrid" style="position:fixed;top:0;left:0;right:0;bottom:0px;z-index:9999;background:url(https://rawgit.com/marcoscv-work/mini-scripts/master/img/4xGridBlue.png)"></div>');
   }
 };
 window.toggleVerticalGrid();
