6937cfeb3354249211b91a60be9b9cd9c52c7e27
Marcos <marcos.castro@liferay.com>
2015-04-10 13:26:36 +0200

    Changing position fixed by absolute, and recalculate body-height

--- grid4x.js
+++ grid4x.js
@@ -5,7 +5,13 @@ window.toggleVerticalGrid = function(){
   	verticalGridNode.remove();
   }
   else {
-  	A.getBody().prepend('<div id="verticalGrid" style="position:fixed;top:0;left:0;right:0;bottom:0px;z-index:9999;background:url(https://cdn.rawgit.com/marcoscv-work/mini-scripts/master/img/4xGridBlue.png)"></div>');
+  	A.getBody().prepend('<div id="verticalGrid" style="position:absolute;top:0;left:0;right:0;bottom:0px;z-index:9999;background:url(https://cdn.rawgit.com/marcoscv-work/mini-scripts/master/img/4xGridBlue.png)"></div>');
+	var bodyHeight = A.getBody().height();
+	if (bodyHeight == undefined) {
+		bodyHeight = document.body.clientHeight;
+	}
+	verticalGridNode = A.one('#verticalGrid');
+	verticalGridNode.setStyle('min-height', bodyHeight);
   }
 };
 window.toggleVerticalGrid();
