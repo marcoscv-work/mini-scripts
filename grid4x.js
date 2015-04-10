b2f53317fe7224a9261bfd939f67ab58b21bb032
Marcos <marcos.castro@liferay.com>
2015-04-10 12:50:02 +0200

    Script 4x Grid

---
+++ grid4x.js
@@ -0,0 +1,11 @@
+A = window.A || (window.AUI && window.AUI());
+window.toggleVerticalGrid = function(){
+  verticalGridNode = A.one('#verticalGrid');
+  if (verticalGridNode){
+  	verticalGridNode.remove();
+  }
+  else {
+  	A.getBody().prepend('<div id="verticalGrid" style="position:fixed;top:0;left:0;right:0;bottom:0px;z-index:9999;background:https://rawgit.com/marcoscv-work/mini-scripts/master/img/4xGridBlue.png"></div>');
+  }
+};
+window.toggleVerticalGrid();
